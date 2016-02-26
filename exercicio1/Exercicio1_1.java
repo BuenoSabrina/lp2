0b6e26e9da1c522e465836a3824b1d964cff29e1
BuenoSabrina <sabrinabueno3@outlook.com>
2016-02-26 08:37:03 -0300

    Update Exercicio1_1.java

--- exercicio1/Exercicio1_1.java
+++ exercicio1/Exercicio1_1.java
@@ -1,10 +1,8 @@
 public class a Exercicio1_1{
 
 public static void main(String [] args){
-	
-String parametro = args[0];
 
-ystem.out.println("o meu nome é " + parametro);
+ystem.out.println("o meu nome é Sabrina");
 	
 	}
 }
